6a7b731271adad866e98958baf542ed6c287b020
humdan123 <36013764+humdan123@users.noreply.github.com>
2018-04-14 02:56:23 -0400

    Add files via upload

---
+++ Hearts_Game.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 17:29:15 2018
+
+@author: humza
+"""
+from Card_Classes import Card, Deck, Player
+
+class Bot(Player):
+    '''
+    A bot for the Hearts Game.
+    '''
+    def __init__(self, name):
+        Player.__init__(self, name, True)
+        
+    #TODO: Create an algorithm for the bot to run
+
+class Game_Hearts:
+    '''
+    Instance of the game of hearts
+    '''
+    def __init__(self, num_players, points_to_end):
+        '''
+        Makes a game instance with num_players amount of players and
+        how many points required to end the game.
+        '''
+        self.num_players = num_players
+        self.points = points_to_end
+        self.players = []
+        n = num_players
+        while n > 0:
+            p = Player('Player {}'.format(n), False)
+            self.players.append(p)
+            n -= 1
+        if len(self.players) < 4:
+            n = 4 - num_players
+            while n < 0:
+                p = Bot('Player {}'.format(n))
+                self.players.append(p)
+                n -= 1
+        self.deck = Deck(52, ['Spades', 'Clubs', 'Hearts', 'Diamonds'])
+        self.deck.fill()
+        self.deck.shuffle()
+    
+    def distribute_cards(self):
+        '''
+        Distribute the cards to all players
+        '''
+        cards_to_deal = self.cards[:]
+        while len(cards_to_deal) > 0:
+            self.players[0].deal(cards_to_deal.pop())
+            self.players[1].deal(cards_to_deal.pop())
+            self.players[2].deal(cards_to_deal.pop())
+            self.players[3].deal(cards_to_deal.pop())
+
+    def play_game(self):
+        '''
+        Starts the game.
+        '''
+        card_pass = ['L', 'R', 'A', 'N']
+        n = 0
+        while (self.players[0].score < self.points) and\
+        (self.players[1].score < self.points) and\
+        (self.players[2].score < self.points) and\
+        (self.players[3].score < self.points):
+            
+            
+
+
+
+if __name__ == '__main__':
+    #TODO: Create the game
+    pass
